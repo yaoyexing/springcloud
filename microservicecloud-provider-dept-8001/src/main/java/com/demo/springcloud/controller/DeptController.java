c0dbf7e6ad4bf77a94014fe886ae5e58a7bc54aa
yaoyexing <2530121801@qq.com>
2020-08-04 17:42:44 +0800

    Eureka增加服务发现

--- microservicecloud-provider-dept-8001/src/main/java/com/demo/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/demo/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.demo.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +20,34 @@ public class DeptController {
 	@Autowired
 	public DeptService deptService;
 
-	@RequestMapping(value="/dept/add", method=RequestMethod.POST)
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept) {
 		return deptService.add(dept);
 	}
 
-	@RequestMapping(value="/dept/get/{id}", method=RequestMethod.GET)
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
 	public Dept get(@PathVariable Long id) {
 		return deptService.get(id);
 	}
 
-	@RequestMapping(value="/dept/list", method=RequestMethod.GET)
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
 	public List<Dept> list() {
 		return deptService.list();
+	}
+
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery() {
+		List<String> list = client.getServices();
+		System.out.println("******" + list);
+		List<ServiceInstance> serviceInstanceList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance serviceInstance : serviceInstanceList) {
+			System.out.println(serviceInstance.getServiceId() + "\t" + serviceInstance.getHost() + "\t"
+					+ serviceInstance.getPort() + "\t" + serviceInstance.getUri());
+		}
+		return this.client;
+
 	}
 }
