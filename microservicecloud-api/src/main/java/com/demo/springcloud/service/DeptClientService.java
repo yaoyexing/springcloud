605c9a49541c23feaae1ee7e0114152c45648ae6
yaoyexing <2530121801@qq.com>
2020-08-09 09:48:46 +0800

    hystrix：服务熔断+服务降级

--- microservicecloud-api/src/main/java/com/demo/springcloud/service/DeptClientService.java
+++ microservicecloud-api/src/main/java/com/demo/springcloud/service/DeptClientService.java
@@ -8,16 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.demo.springcloud.entities.Dept;
+import com.demo.springcloud.fallbackFactory.DeptClientServiceFallbackFactory;
 
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
-public interface DeptClientService
-{
-  @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
-  public Dept get(@PathVariable("id") long id);
- 
-  @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
-  public List<Dept> list();
- 
-  @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
-  public boolean add(Dept dept);
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT", fallbackFactory = DeptClientServiceFallbackFactory.class)
+public interface DeptClientService {
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	public Dept get(@PathVariable("id") long id);
+
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list();
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+	public boolean add(Dept dept);
 }
--- microservicecloud-consumer-dept-80/src/main/java/com/demo/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/demo/springcloud/cfgbeans/ConfigBean.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import com.demo.myRule.MyIRule;
-import com.netflix.loadbalancer.IRule;
-
 @Configuration
 public class ConfigBean {
 
